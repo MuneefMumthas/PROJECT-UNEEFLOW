7f6df3e75614a66c85f28b07734a68ebc6483053
MuneefMumthas <22206529@bucks.ac.uk>
2025-03-16 18:41:23 +0000

    Set up the tkinter environment

---
+++ main.py
@@ -0,0 +1,11 @@
+import sys
+import tkinter as tk
+from tkinter import filedialog, messagebox
+
+#creating the main window
+root = tk.Tk()
+root.title("ML Pipeline Application")
+root.geometry("800x800")  #window size
+
+#eunning the main window
+root.mainloop()
