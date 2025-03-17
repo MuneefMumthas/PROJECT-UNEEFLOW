bf7b5cdd7831f8f96a25f83ecb38c960b7f7deb4
MuneefMumthas <22206529@bucks.ac.uk>
2025-03-17 17:01:37 +0000

    Added a way to lock the window size to default

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def center_window(window, width, height):
     x_pos = (screen_width - width) // 2
     y_pos = (screen_height - height) // 2
     window.geometry(f"{width}x{height}+{x_pos}+{y_pos}")
-
+    window.resizable(False, False)
 
 
 #creating the main window
